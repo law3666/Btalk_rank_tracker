2629c41375baa9ccf41e9f51c8f41737aa1e2bbf
Abdullahi <abdullahimohammed484@gmail.com>
2025-07-14 22:33:51 +0100

    Add home route for Render root URL

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
 import requests
 from bs4 import BeautifulSoup
@@ -7,6 +7,9 @@ import re
 app = Flask(__name__)
 CORS(app)
 
+@app.route('/')
+def home():
+    return render_template('index.html')
 # Define Bitcointalk rank thresholds
 RANKS = [
     {"name": "Brand New", "activity": 0, "merit": 0},
